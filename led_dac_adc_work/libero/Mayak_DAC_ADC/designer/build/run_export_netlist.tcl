72f0e0258ab63bd94f4fe8f2e52ee5d3e45b784f
nikellanjilo99@gnail.com <nikellanjilo99@gnail.com>
2017-06-20 22:54:38 +0300

    Changed all directories

--- led_dac_adc_work/libero/Mayak_DAC_ADC/designer/build/run_export_netlist.tcl
+++
@@ -1,4 +0,0 @@
-set_device -fam IGLOO2
-read_edif  \
-    -file {C:\Projects\Verilog\led_IGLOO\led_dac_adc_qudr\led_dac_adc_work\libero\Mayak_DAC_ADC\synthesis\synthesis_1\build.edn}
-write_verilog -file {C:\Projects\Verilog\led_IGLOO\led_dac_adc_qudr\led_dac_adc_work\libero\Mayak_DAC_ADC\synthesis\synthesis_1\build.v}
--- verilog_conv/README.txt
+++
@@ -1,1 +0,0 @@
-Here will be verilog code of FPGA realization of convolution 
